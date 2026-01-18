8de62499f49c9f02e3879d06e342a0f56350cb68
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-01-18 22:32:49 +0100

    remote db configured

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const testConnection = async () => {
